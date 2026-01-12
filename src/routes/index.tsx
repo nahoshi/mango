32f8b1cf5b7ab99fa58f08abbdaeb60eab9cb7b7
Bocchi <bocchi@keemail.me>
2026-01-12 00:52:05 -0300

    setup: Start the tanstack project with shadcn and biome.

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: App
+})
+
+export function App() {
+  return <h1>Risia linda</h1>
+}
